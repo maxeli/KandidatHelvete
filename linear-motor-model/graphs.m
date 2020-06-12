93e8b5face1c79deb9c0d5e1b12e2bf5e998060e
Rikard Legge <rikard.legge@gmail.com>
2020-06-12 21:18:33 +0200

    Post inlämning

--- linear-motor-model/graphs.m
+++ linear-motor-model/graphs.m
@@ -22,8 +22,8 @@ for freq=unique(f)'
     y_freq = y(f==freq,:);
     for i=1:size(y_freq,2)
         o = (i-2);
-        y_wind = abs(y_freq(:,i));
-        if max(y_wind) > 20
+        y_wind = -y_freq(:,i);
+        if max(abs(y_wind)) > 20
             y_mean(f_i,i) = NaN;
             continue;
         end
@@ -47,13 +47,121 @@ ylabel("Kraft (N)");
 axis([0 100 0 10]);
 print("./img/sim-frekvens-kraft", '-dpng');
 
+%% Simulering 10-100 Hz eco
+clear all;clf;
+fig = figure(1);
+set(fig, 'Position', [0 0 500 200]);
+hold on;
+
+T = readtable('10-100 cgf 3.csv','Format','%f%f%f%f%f%f%f%f', 'HeaderLines',5);
+
+f = T.Var2;
+t = T.Var3;
+y = -T.Var8;
+
+leg = [];
+for freq=fliplr(unique(f)')
+    plot(t(f==freq).*f(f==freq)*2,y(f==freq));
+    leg = [leg sprintf("%d Hz",freq)];
+end
+
+
+legend(leg);
+xlabel("Perioder");
+ylabel("Kraft (N)");
+axis([0 4 0 10]);
+print("./img/sim-frekvens-kraft-eco14", '-dpng');
+
+%% Simulering instabilitet
+
+clear all;clf;
+fig = figure(1);
+set(fig, 'Position', [0 0 250 150]);
+hold on;
+
+T = readtable('10-100 cgf 3.csv','Format','%f%f%f%f%f%f%f%f', 'HeaderLines',5);
+
+f = T.Var2;
+t = T.Var3;
+% 3 2 1 eco12 eco14
+y = [T.Var4,T.Var8];
+
+markers = ["o","s"];
+color = ["blue", "magenta"];
+
+y_mean = zeros(size(unique(f),1),size(y,2));
+
+f_i = 1;
+for freq=unique(f)'
+    x = f(f==freq);
+    y_freq = y(f==freq,:);
+    for i=1:size(y_freq,2)
+        o = (i-2);
+        y_wind = -y_freq(:,i);
+        plot(x(11:end)+o,y_wind(11:end), color(i),'HandleVisibility','off');
+        y_mean(f_i,i) = mean(y_wind(11:end));
+    end
+    f_i = f_i+1;
+end
+
+for i=1:size(y_mean,2)
+    o = (i-2);
+    plot(unique(f)'+o,y_mean(:,i), append(color(i),"-", markers(i)));
+end
+
+%title("Vågform på spänning över spolarna");
+%axis([15/sample 75/sample -0.8 0.8]);
+lgd = legend(["3 Lager","Ekonomisk 14"]);
+lgd.Location = 'northwest';
+xlabel("Frekvens (Hz)");
+ylabel("Kraft (N)");
+print("./img/sim-unstable", '-dpng');
+
+%% Simulering instabilitet 1 frekvens
+
+clear all;clf;
+fig = figure(1);
+set(fig, 'Position', [0 0 250 150]);
+hold on;
+
+T = readtable('10-100 cgf 3.csv','Format','%f%f%f%f%f%f%f%f', 'HeaderLines',5);
+
+f = T.Var2;
+t = T.Var3;
+y = -T.Var4;
+
+markers = ["o","s"];
+color = ["blue", "magenta"];
+
+freq = 60;
+tf = t(f==freq);
+yf = y(f==freq);
+
+
+yyaxis left; hold on;
+plot(tf, yf, append("-", markers(1)));
+ylabel("Kraft (N)");
+axis([0 0.034 4 6]);
+
+yyaxis right; hold on;
+plot(tf, yf, append("-", markers(2)));
+ylabel("Kraft (N)");
+axis([0 0.034 -1500 2500]);
+
+
+lgd = legend(["3 Lager, 60 Hz, inzoomad","3 Lager, 60 Hz, utzoomad"]);
+lgd.Location = 'southeast';
+xlabel("Frekvens (Hz)");
+ylabel("Kraft (ln N)");
+print("./img/sim-unstable+single", '-dpng');
+
 %% Signal Spänning
 clear all;clf;hold on;
 dsample = 10;
 sample = 10000/dsample;
 
 fig = figure(1);
-set(fig, 'Position', [0 0 500 200]);
+set(fig, 'Position', [0 0 500 100]);
 
 T = readtable('signal-1v.csv', 'HeaderLines',29);
 t = downsample(T.Var2,1);
@@ -82,7 +190,7 @@ print("./img/mes-sig-spanning", '-dpng');
 %% Kalibrering lastcell
 clear all;clf;
 fig = figure(1);
-set(fig, 'Position', [0 0 500 200]);
+set(fig, 'Position', [0 0 500*0.65 200*0.65]);
 hold on;
 T = readtable('Mätresultat - Lastcell.csv', 'HeaderLines',2);
 y = [0; T.Var12(1:21)]/1000;
@@ -159,20 +267,21 @@ t = T.Var5(47+10:68);
 f = str2double(strrep(T.Var12(47+10:68),",","."));
 
 yyaxis left; hold on;
-plot(t,f,"-o");
+plot(temp,f,"-o");
 ylabel("Kraft (N)");
 commas();
 
 yyaxis right; hold on;
-plot(t,temp,"-x");
-ylabel("Temperatur (ºC)");
+plot(temp,i,"-x");
+ylabel("Ström (A)");
+axis([35 70 8.5 9]);
 
-xlabel("Tid (s)");
-lgd = legend(["Uppmätt kraft", "Uppmätt Temperatur"]);
+xlabel("Temperatur (ºC)");
+lgd = legend(["Uppmätt kraft", "Uppmätt Ström (A RMS)"]);
 lgd.Location = 'north';
 print("./img/mes-temp-kraft", '-dpng');
 
-%% Frekvensanalys
+%% Frekvensanalys Upmätt
 clear all;clf;
 fig = figure(1);
 set(fig, 'Position', [0 0 500 200]);
@@ -185,7 +294,7 @@ forc = T.Var9/1000;
 prevF = freq(1);
 start = 1;
 leg = [];
-markers = ["o","x","+","*",".","s","d","^","v",">","<","p","h" ];
+markers = ["o","x","+","*","s","d","^","v",">","<","p","h" ];
 n = 1;
 for i=1:length(freq)
     f = freq(i);
@@ -202,4 +311,74 @@ lgd.Location = 'northwest';
 
 xlabel("Ström (A)");
 ylabel("Kraft (N)");
+axis([0 7 0 12]);
 print("./img/mes-frekvens-kraft", '-dpng');
+
+%% Frekvensanalys Simulerad
+clear all;clf;
+fig = figure(1);
+set(fig, 'Position', [0 0 500 200]);
+hold on;
+
+T = readtable('ström cfg 3 10-60 Hz.csv','Format','%f%f%f%f%f%f%f%f%f', 'HeaderLines',5);
+f = T.Var2;
+a = T.Var3;
+t = T.Var4;
+y = -T.Var9;
+
+markers = ["o","x","+","*","s","d","^","v",">","<","p","h" ];
+color = ["blue", "green", "red", "cyan", "magenta"];
+
+f_i = 2;
+leg = [];
+plot(0, 0,append("-", markers(1)));
+leg = [leg sprintf("5 Hz")];
+for freq=unique(f)'
+    if freq > 60
+        break;
+    end
+    y_freq = y(f==freq,:);
+    a_freq = a(f==freq);
+    a_means = zeros(size(unique(a_freq),1),1);
+    a_i = 1;
+    for amp=unique(a_freq)'
+        i = logical((f==freq) .* a==amp);
+        y_a = y(i,:);
+        if max(abs(y(i))) > 4
+            a_means(a_i) = NaN;
+        else 
+            a_means(a_i) = mean(y_a(11:end));
+        end
+        a_i = a_i + 1;
+    end
+    plot(unique(a_freq)',a_means,append("-", markers(f_i)));
+    leg = [leg sprintf("%d Hz",freq)];
+    f_i = f_i+1;
+end
+
+lgd = legend(leg);
+lgd.Location = 'northwest';
+xlabel("Ström (A)");
+ylabel("Kraft (N)");
+axis([0 7 0 4]);
+print("./img/sim-frekvens-strom-kraft", '-dpng');
+
+%% Mätosäkerhet
+clear all;clf;hold on;
+sample = 10000;
+
+fig = figure(1);
+set(fig, 'Position', [0 0 500 200]);
+
+T = readtable('temp-force.csv', 'HeaderLines',29);
+y = [T.Var3(1*sample:3*sample-1); T.Var3(20*sample:22*sample)];
+t = linspace(0,4,length(y));
+plot(t,y);
+%title("Vågform på spänning över spolarna");
+axis([0 4 1 1.13]);
+lgd = legend(["Lastcell"]);
+lgd.Location = 'southeast';
+ylabel("Spänning (V)");
+xlabel("Tid (s)");
+commas();
+print("./img/mes-osäkerhet", '-dpng');
